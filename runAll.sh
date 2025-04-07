195f188fc5a9cc7c633e10cef9bf91b5b463142f
Nandita J <nanditajayanthi1@gmail.com>
2025-04-07 16:41:31 -0500

    updating runAll

--- runAll.sh
+++ runAll.sh
@@ -1,18 +1,22 @@
 while read line; do
+    if [[ ${line} =~ ^\# ]]; then
+       #echo "line starts with Hash"
+       continue
+    fi
     slug=$(echo $line | cut -d',' -f1 )
     sha=$(echo $line | cut -d',' -f2)
     module=$(echo $line | cut -d',' -f3)
-    testName=$(echo $line | cut -d',' -f4)
+    test_name=$(echo $line | cut -d',' -f4)
     
     git clone https://github.com/$slug input/$slug
     cd input/$slug
     git checkout $sha
-    mvn clean install -U -DskipTests=true
-    
+    mvn clean install -pl $module -am -U -DskipTests
+     
+    echo "mvn edu.utexas.ece:flakesync-maven-plugin:1.0-SNAPSHOT:flakefind -Dflakesync.testName=${test_name} -pl $module"
     #find . -name "*.class" | grep -v Tests | sed 's;.*target/classes/;;'| sed 's;/;.;g' | sed 's;.class$;;' > $module/.flakesync/whitelist.txt
     mvn edu.utexas.ece:flakesync-maven-plugin:1.0-SNAPSHOT:flakefind -Dflakesync.testName=${test_name} -pl $module
     mvn edu.utexas.ece:flakesync-maven-plugin:1.0-SNAPSHOT:flakedelay -Dflakesync.testName=${test_name} -pl $module
-    
     mvn edu.utexas.ece:flakesync-maven-plugin:1.0-SNAPSHOT:flakedeltadebug -Dflakesync.testName=${test_name} -pl $module
     mvn edu.utexas.ece:flakesync-maven-plugin:1.0-SNAPSHOT:critsearch -Dflakesync.testName=${test_name} -pl $module
     mvn edu.utexas.ece:flakesync-maven-plugin:1.0-SNAPSHOT:barrierpointsearch -Dflakesync.testName=${test_name} -pl $module
