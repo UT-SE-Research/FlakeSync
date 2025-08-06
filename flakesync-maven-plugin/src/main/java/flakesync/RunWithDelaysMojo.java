82f81b7fd2e390273672def3f5418b0570ebd2df
Nandita J <nanditajayanthi1@gmail.com>
2025-08-06 11:54:54 -0700

    added run everywhere

--- flakesync-maven-plugin/src/main/java/flakesync/RunWithDelaysMojo.java
+++ flakesync-maven-plugin/src/main/java/flakesync/RunWithDelaysMojo.java
@@ -1,3 +1,32 @@
+/*
+The MIT License (MIT)
+Copyright (c) 2015 Alex Gyori
+Copyright (c) 2022 Kaiyao Ke
+Copyright (c) 2015 Owolabi Legunsen
+Copyright (c) 2015 Darko Marinov
+Copyright (c) 2015 August Shi
+
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 package flakesync;
 
 import flakesync.common.ConfigurationDefaults;
@@ -20,7 +49,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Mojo(name = "flakedelay", defaultPhase = LifecyclePhase.TEST, requiresDependencyResolution = ResolutionScope.TEST)
+@Mojo(name = "delaylocs", defaultPhase = LifecyclePhase.TEST, requiresDependencyResolution = ResolutionScope.TEST)
 public class RunWithDelaysMojo extends FlakeSyncAbstractMojo {
 
     int[] delays = {100, 200, 400, 800, 1600, 3200, 6400, 12800};
@@ -29,12 +58,12 @@ public class RunWithDelaysMojo extends FlakeSyncAbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         super.execute();
         Logger.getGlobal().log(Level.INFO, ("Running RunWithDelaysMojo"));
+        MojoExecutionException allExceptions = null;
 
-        //Create the whitelist file that is passed to the agent
         createWhiteList();
 
         for (int i = 0; i < delays.length; i++) {
-            CleanSurefireExecution cleanExec = new CleanSurefireExecution(
+            SurefireExecution cleanExec = SurefireExecution.SurefireFactory.createDelayAllExec(
                     this.surefire, this.originalArgLine, this.mavenProject,
                     this.mavenSession, this.pluginManager,
                     Paths.get(this.baseDir.getAbsolutePath(), ConfigurationDefaults.DEFAULT_FLAKESYNC_DIR).toString(),
@@ -52,7 +81,7 @@ public class RunWithDelaysMojo extends FlakeSyncAbstractMojo {
 
 
     private MojoExecutionException executeSurefireExecution(MojoExecutionException allExceptions,
-                                                            CleanSurefireExecution execution) throws MojoExecutionException,
+                                                            SurefireExecution execution) throws MojoExecutionException,
             Throwable {
         execution.run();
         return allExceptions;
@@ -64,8 +93,7 @@ public class RunWithDelaysMojo extends FlakeSyncAbstractMojo {
 
     private boolean createWhiteList() {
         System.out.println("Inside createWhiteList");
-        File whitelist = new File(Paths.get(ConfigurationDefaults.DEFAULT_FLAKESYNC_DIR,
-                ConfigurationDefaults.WHITELIST_FILE).toString());
+        File whitelist = new File(this.mavenProject.getBasedir() + "/.flakesync/whitelist.txt");
         File outputDir = new File(this.mavenProject.getBuild().getOutputDirectory());
 
         try {
