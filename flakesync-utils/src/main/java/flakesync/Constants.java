43b16fa328f59a48b72a17897f166725fd3d0393
Nandita J <nanditajayanthi1@gmail.com>
2025-09-08 11:02:09 -0500

    new paths added to constants

--- flakesync-utils/src/main/java/flakesync/Constants.java
+++ flakesync-utils/src/main/java/flakesync/Constants.java
@@ -1,5 +1,6 @@
 package flakesync;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -11,6 +12,9 @@ public class Constants {
     public static final String LOCATIONS_FILE = "Locations.txt";
     public static final String LOCATIONS_MIN_FILE = "Locations_minimized.txt";
     public static final String LOCATIONS_TEMP_FILE = "Locations_temp.txt";
+    public static final String ROOTS_DIR = "Locations";
+    public static final String STACKTRACE_FILE = "StackTrace.txt";
+    public static final String ROOT_METHOD_FILE = "Root.txt";
     public static final String WHITELIST_FILE = "whitelist.txt";
 
 
@@ -46,5 +50,19 @@ public class Constants {
     public static Path getWorkingLocationsFilepath(String testName) {
         String fileName = testName.replace("#", ".") + "-" + LOCATIONS_TEMP_FILE;
         return Paths.get(".", DEFAULT_FLAKESYNC_DIR, fileName);
+    }
+
+    public static Path getStackTraceFilepath(String testName) {
+        String fileName = testName.replace("#", ".") + "-" + STACKTRACE_FILE;
+        return Paths.get(".", DEFAULT_FLAKESYNC_DIR, fileName);
+    }
+
+    public static Path getRootMethodFilepath(String testname) {
+        String fileName = testname.replace("#", ".") + "-" + ROOT_METHOD_FILE;
+        File rootsDir = new File(String.valueOf(Paths.get(DEFAULT_FLAKESYNC_DIR, ROOTS_DIR)));
+        if(!rootsDir.exists()) {
+            rootsDir.mkdirs();
+        }
+        return Paths.get(".", DEFAULT_FLAKESYNC_DIR, ROOTS_DIR, fileName);
     }
 }
